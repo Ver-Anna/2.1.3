9d1377a1d2e3002b12f981b54613eee3162b9987
Ver-Anna <159172994+Ver-Anna@users.noreply.github.com>
2024-04-01 19:23:15 +0700

    Add files via upload

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,32 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+
+@Component
+public class AnimalsCage {
+    private Animal animal;
+    private Timer timer;
+    @Autowired
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.timer = timer;
+        this.animal = animal;
+    }
+
+
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+}
